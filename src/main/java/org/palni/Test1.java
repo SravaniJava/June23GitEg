86da447ba33c44b4b3c31589c7239f582f82b41c
sravaniPal <sravanigoudjava.com>
2025-07-23 19:06:07 +0530

    changes in Test1 added by Master second time

--- src/main/java/org/palni/Test1.java
+++ src/main/java/org/palni/Test1.java
@@ -4,7 +4,7 @@ package org.palni;
 
 public class Test1 {
     public static void test(){
-        System.out.println("chages are done by the master");
+        System.out.println("chages are done by the master sevem five");
         System.out.println("this is sravani test first time");
         System.out.println("this are changes done by the sravani");
     }
