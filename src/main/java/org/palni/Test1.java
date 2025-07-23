ced3772031afcf22811880c25527517ec6bd6700
sravaniPal <sravanigoudjava.com>
2025-07-23 22:29:51 +0530

    changes in Test1 modified by Master  time  by sravani

--- src/main/java/org/palni/Test1.java
+++ src/main/java/org/palni/Test1.java
@@ -7,6 +7,6 @@ public class Test1 {
         System.out.println("this is sravani test first time seven nine");
         System.out.println("chages are done by the master sevem five");
         System.out.println("this is sravani test first time");
-        System.out.println("this are changes done by the sravaniiii");
+        System.out.println("this are changes done by the sravaniiii123");
     }
 }
