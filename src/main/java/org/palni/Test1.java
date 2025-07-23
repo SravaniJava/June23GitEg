768929a0e782f5af11860eeadbf8cdc334c36346
sravaniPal <sravanigoudjava.com>
2025-07-23 22:35:50 +0530

    changes in Test1  one line is added by sravani

--- src/main/java/org/palni/Test1.java
+++ src/main/java/org/palni/Test1.java
@@ -8,5 +8,6 @@ public class Test1 {
         System.out.println("chages are done by the master sevem five");
         System.out.println("this is sravani test first time");
         System.out.println("this are changes done by the sravaniiii123");
+        System.out.println("addedd for test by sravani");
     }
 }
