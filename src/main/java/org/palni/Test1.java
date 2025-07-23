ea8f7d67841bfa25073abd24eb291043bd1ab3b5
sravaniPal <sravanigoudjava.com>
2025-07-23 18:57:01 +0530

    changes in Test1 added by Master

--- src/main/java/org/palni/Test1.java
+++ src/main/java/org/palni/Test1.java
@@ -4,6 +4,7 @@ package org.palni;
 
 public class Test1 {
     public static void test(){
+        System.out.println("chages are done by the master");
         System.out.println("this is sravani test first time");
     }
 }
